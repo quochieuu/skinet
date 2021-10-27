d7923a9d42e31229e6aa689d739dc9c9aecad3ed
Hieu Ho <quochieuu@gmail.com>
2021-10-27 20:17:10 +0700

    Ađ notfound, error component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -6,7 +6,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { ShopModule } from './home/home.module';
+import { ErrorInterceptor } from './core/interceptors/error.interceptor';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,9 @@ import { ShopModule } from './home/home.module';
     CoreModule,
     HomeModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -1,13 +1,21 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
+import { ToastrModule } from "ngx-toastr";
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
+import { ServerErrorComponent } from "./server-error/server-error.component";
+import { TestErrorComponent } from "./test-error/test-error.component";
 
 @NgModule({
-    declarations: [NavBarComponent],
+    declarations: [NavBarComponent, TestErrorComponent, NotFoundComponent, ServerErrorComponent],
     imports: [
         CommonModule,
-        RouterModule
+        RouterModule,
+        ToastrModule.forRoot({
+            positionClass: 'toastr-bottom-right',
+            preventDuplicates: true
+        })
     ],
     exports: [NavBarComponent]
 })
